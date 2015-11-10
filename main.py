6a93c4402e38954f97154e9faf5f544e486af9af
Austin Scott <xeirxes@gmail.com>
2015-11-10 01:51:24 -0800

    fleshed out the Site class, rearranged things a bit, began working on a launch function which populates the site with the beginning musicians

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 class Site:
     def __init__(self):
+
         # Initialize empty lists to hold users and songs
         self.users = list()
         self.songs = list()
 
         # Keeps track of site stats
-        stats = {
-            "storage": 0,
-            "users": 0,
+        self.stats = {
+
+            # This keeps track of total amount stored, and also how many mbs per day have been used since the beginning
+            "mbs_storage": 0,
+            "days_per_mbs_used": 0,
+
+            # Bandwidth costs
+            "mbs_upload": 0,
+            "mbs_download": 0,
+
+            # Users
+            "total_users": 0,
             "musicians": 0,
+            "promoters": 0,
+            "fans": 0,
             "songs": 0,
-            "income": 0
+
+            # Finances
+            "gross_sales": 0,
+            "musician_payouts": 0,
+            "promoter_payouts": 0
         }
+
+    def launch(self, num_musicians):
+        for i in range(num_musicians):
+            self.users.append(Musician())
+
+class User:
+    def __init__(self):
+        pass
+
+class Musician(User):
+    def __init__(self):
+        pass
