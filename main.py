c286d1009be0f18bc83a4ace2964285058331fa4
xeirxes <xeirxes@gmail.com>
2015-11-17 15:27:47 -0800

    cleared out a lot of the early ideas I had, packaged stats into classes instead so that the usage of them is much cleaner.

--- main.py
+++ main.py
@@ -37,37 +37,17 @@ class Site:
         for i in range(num_musicians):
             self.users.append(Musician())
 
-def nv(mu, sigma, low_cut, high_cut):
-    #This helps to filter out random values we don't want.
-    #It uses the normal variate function of the random library.
-    a = -1
-    while (a < low_cut) or (a > high_cut):
-        a = random.normalvariate(mu, sigma)
-    return a
-
-def test(percent_chance):
-    return (random.random() <= percent_chance)
-
 class User:
-    def __init__(self, *arg):
-        if len(arg) == 0:
-            a = self
-        else:
-            a = arg[0]
-        a.library = list()
-        a.audience = round(nv(400, 250, 0, 2000)) #random number near 400
-        a.popularity = nv(0.075, 0.1, 0.001, 1) #percent chance audience reacts to thing
-        a.sharer = nv(0.25, 0.25, 0, 1) #percent chance this person will decide to share something they buy
-        a.sense = nv(0.3, 0.1, 0.05, 0.95) #percent chance this person recognizes song quality
-        a.pickiness = nv(0.8, 0.2, 0.2, 1.9) #cutoff for what quality of song they will buy
+    def __init__(self, upline):
+        self.stats = UserStats()
 
-class Musician(User):
+class UserStats():
     def __init__(self):
-        super().__init__(self)
-        self.skill = nv(0.5, 0.15, 0, 1) #adds to the quality of tracks
-        self.talent = nv(0.5, 0.25, 0, 1) #a random modifier is added to every track, limited by talent
-        self.writing_frequency = round(nv(60, 60, 1, 730)) #how many days, on average, before new material is released
-        self.starting_set = round(nv(3, 1, 1, 8)) #how many songs the Musician has in the beginning of his signup
+        pass
+
+class Stat():
+    def __init__(self, mu, sigma, min, max):
+        pass
 
 class Song:
     def __init__(self, creator, quality):
