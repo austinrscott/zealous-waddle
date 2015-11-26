8dbca552cc459dcff029e25368890f1a43d0d652
Austin Scott <xeirxes@gmail.com>
2015-11-26 13:27:59 -0800

    changed musician/promoter split from a percent ratio to a decimal amount (which will all be changed later when it links to Song.cost), site now adds song to song list of author when song is added, users now have 'expenses'. sale function now uses a rolling balance style system. distributed the sale function work to 5 new functions which manage paying users and keeping records for each part of the sale. The fifth function is a recursive commission function to handle the split of income to multiple users up the line.

--- site.py
+++ site.py
@@ -17,18 +17,19 @@ class Site:
         }
         self.settings = {
             'site_fee': 0.15,
-            'musician_rate': 0.3,
-            'promoter_rate': 0.7,
-            'upline_rate': 0.6
+            'musician_fee': 0.25,
+            'upline_ratio': 0.6,
         }
 
     def add_song(self, song, author):
         self.songs.append(song)
+        self.users[author]['songs'][song] = author
         self.figures['uploads_mbs'] += song.size
 
     def add_user(self, user):
         self.users[user] = {
             'income': 0,
+            'expenses': 0,
             'songs': {},
         }
 
@@ -45,13 +46,39 @@ class Site:
         self.figures['downloads_mbs'] += 1
 
     def sale(self, song, promoter, buyer):
-        self.figures['site_fees']
-        self.figures['promoter_payouts']
-        self.figures['musician_payouts']
-        self.users[promoter]['income'] +=
-        # TODO: Finish sale function
+        balance = self.buy_song(song, promoter, buyer)
+        balance -= self.pay_site()
+        balance -= self.pay_musician(song.author)
+        self.commission_chain(song, promoter, balance)
 
-    def commission_chain(self, song, ):
-        # TODO: Finish recursive commission chain function
+    def buy_song(self, song, promoter, buyer):
+        self.users[buyer]['expenses'] += song.cost
+        self.users[buyer]['songs'][song] = promoter
+        self.figures['sales'] += 1
+        self.figures['sales_dollars'] += song.cost
+        return song.cost
+
+    def pay_site(self):
+        self.figures['site_fees'] += self.settings['site_fee']
+        return self.settings['site_fee']
+
+    def pay_musician(self, musician):
+        self.users[musician]['income'] += self.settings['musician_fee']
+        self.figures['musician_payouts'] += self.settings['musician_fee']
+        return self.settings['musician_fee']
+
+    def pay_promoter(self, promoter, amount):
+        upline_slice = amount * self.settings['upline_ratio']
+        self.users[promoter]['income'] += amount - upline_slice
+        self.figures['promoter_payouts'] += amount - upline_slice
+        return upline_slice
+
+    def commission_chain(self, song, promoter, balance):
+        if promoter is song.author:
+            self.users[song.author]['income'] += balance
+            self.figures['musician_payouts'] += balance
+        else:
+            new_balance = self.pay_promoter(promoter, balance)
+            self.commission_chain(song, self.users[promoter]['songs'][song], new_balance)
 
 _site = Site()
